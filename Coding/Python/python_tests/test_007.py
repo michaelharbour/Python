1acb4de9627041b18c32654cfed39c77edc2e2df
michaelharbour <michaelharbour@gmail.com>
2018-10-08 13:54:33 -0700

    moving up test scritp from windows10

--- Coding/Python/python_tests/test_007.py
+++ Coding/Python/python_tests/test_007.py
@@ -1,35 +1,9 @@
-'''This is a general test page to jot down basic python scirpts and test
-while reading through various documentation, turtorial sites, etc.
+usa_gold = 50
+ussr_gold = 24
+romainia_gold = 1
+total_gold = usa_gold + ussr_gold + romainia_gold
+print(total_gold)
+romainia_gold += 1
+print(total_gold)
 
-adding one additional change.  Adding one more change'''
-
-# usa_gold = 50
-# ussr_gold = 24
-# romainia_gold = 1
-
-# total_gold = usa_gold + ussr_gold + romainia_gold
-# print(total_gold)
-
-# romainia_gold += 1
-# print(total_gold)
-
-# Geek Gurl Fortune Teller Program
-
-import random
-import time
-
-print("Welcome to the Fortune teller \n")
-
-question = input("would you like me to tell you your fortune? \n")
-
-time.sleep(2)
-
-fortunes = ['You are a winner' ,
-            'A secret admirer will soon send you a sing of affection' ,
-            'The one that you love is closer than you thing.' ,
-            'Good things will happen to you.' ,
-            'You will do great in life' ,
-            'If you get a Rasberry Pi, the Geek Gods will shine on you',
-            'Don\'t look at me, I am just a computer program and have no mythical powers']
-
-            print(random.choice(fortunes))
+print('\n\nJack Russel Terriers are not that smart.')
